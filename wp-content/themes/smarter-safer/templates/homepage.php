be959587e450def76ce09da6e28f56d7e0e78900
Eric Sherred <eric@ericsherred.com>
2019-05-30 15:34:38 -0400

    cleanup

--- wp-content/themes/smarter-safer/templates/homepage.php
+++ wp-content/themes/smarter-safer/templates/homepage.php
@@ -2,14 +2,14 @@
 
 <?php the_post() ?>
 
-<?php //$featured_img_url = get_the_post_thumbnail_url(); ?>
+<?php $featured_img_url = get_the_post_thumbnail_url(); ?>
 
 <?php get_header(); ?>
 
 <main>
 	<section class="header home">
 		<div class="feature-image">
-			<img src="<?php echo get_stylesheet_directory_uri() ?>/assets/img/flood.jpg" alt="<?php the_title(); ?>">
+			<img src="<?php echo $featured_img_url ?>" alt="<?php the_title(); ?>">
 		</div>
 		<h2>Dangerous weather events and natural disasters are predicted to only get worse.</h2>
 		<div class="button"><a href="#">Download our Congressional Guide <i class="fas fa-arrow-right"></i></a></div>
@@ -44,7 +44,7 @@
 	<section class="featured">
 		<h2>Current Work</h2>
 		<div class="featured-image">
-			<img src="//unsplash.it/600/600" alt="">
+			<img src="<?php echo get_stylesheet_directory_uri() ?>/assets/img/fire.jpg" alt="">
 		</div>
 		<div class="content">
 		‘How to Reform US Disaster Policy to Prepare for a Coming Century of Crisis’ lays out a roadmap to a more rational approach to federal disaster policies that will save taxpayer dollars, protect the environment, and better prepare Americans for the risks they face now – and the risks natural disasters will pose in the future. 
@@ -65,14 +65,14 @@
 				<div class="news">
 					<a target="_blank" href="<?php the_field('link', $item->ID) ?>">
 						<h3><small>In The News</small><?php echo $item->post_title; ?></h3>
-						<img src="//unsplash.it/400/400" alt="">
+						<img src="<?php echo get_the_post_thumbnail_url($item->ID) ?>" alt="">
 					</a>
 				</div>
 			<?php else: ?>
 				<div class="news">
 					<a href="<?php the_permalink($item->ID) ?>">
 						<h3><small>Press Release</small><?php echo $item->post_title; ?></h3>
-						<img src="//unsplash.it/400/400" alt="">
+						<img src="<?php echo get_the_post_thumbnail_url($item->ID) ?>" alt="">
 					</a>
 				</div>
 			<?php endif; ?>
