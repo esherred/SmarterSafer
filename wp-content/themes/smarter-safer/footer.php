e52a9ba9164326fd7e8e1182d7a8e31a8f9722cd
Eric Sherred <eric@ericsherred.com>
2019-05-30 23:12:52 -0400

    footer name

--- wp-content/themes/smarter-safer/footer.php
+++ wp-content/themes/smarter-safer/footer.php
@@ -1,10 +1,18 @@
 	<footer>
 		<div class="inner">
 			<div class="nav">
-				<ul>
-					<li><a href="#">Privacy Policy</a></li>
-					<li><a href="#">Contact Us</a></li>
-				</ul>
+				<?php
+        wp_nav_menu([
+          'menu'            => 'footer',
+          'theme_location'  => 'footer',
+          'container'       => false,
+          'container_id'    => false,
+          'container_class' => false,
+          'menu_id'         => false,
+          'menu_class'      => 'nav',
+          'depth'           => 1
+        ]);
+			?>
 			</div>
 			<div class="disclaimer">
 				Copyright &copy; 2019 SmarterSafer.org
