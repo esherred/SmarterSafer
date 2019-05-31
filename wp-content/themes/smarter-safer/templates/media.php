bb9eb873540521b6bd91308bb3d1f5859a18429b
Eric Sherred <eric@ericsherred.com>
2019-05-30 22:50:31 -0400

    media page

---
+++ wp-content/themes/smarter-safer/templates/media.php
@@ -0,0 +1,90 @@
+<?php /* Template Name: Media Page */ ?>
+
+<?php get_header(); ?>
+
+<main>
+	<section class="header news blue">
+		<div class="container">
+			<h1>Latest News</h1>
+		</div>
+	</section>
+	
+	<section class="news-items">
+		<div class="container">
+			<div class="news-item">
+				<a href="#">
+					<h3>Media Item</h3>
+					<img src="https://images.unsplash.com/photo-1544513566-b4de3719ee70?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" alt="">
+				</a>
+			</div>
+			<div class="news-item">
+				<a href="#">
+					<h3>Media Item</h3>
+					<img src="https://images.unsplash.com/photo-1543398355-bb0d4b80ae22?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2711&q=80" alt="">
+				</a>
+			</div>
+			<div class="news-item">
+				<a href="#">
+					<h3>Media Item</h3>
+					<img src="https://images.unsplash.com/photo-1548183375-5eaf88925e19?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80" alt="">
+				</a>
+			</div>
+			<div class="news-item">
+				<a href="#">
+					<h3>Media Item</h3>
+					<img src="https://images.unsplash.com/photo-1544257750-572358f5da22?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1988&q=80" alt="">
+				</a>
+			</div>
+			<div class="news-item">
+				<a href="#">
+					<h3>Media Item</h3>
+					<img src="https://images.unsplash.com/photo-1473260079709-83c808703435?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2679&q=80" alt="">
+				</a>
+			</div>
+			<div class="news-item">
+				<a href="#">
+					<h3>Media Item</h3>
+					<img src="https://images.unsplash.com/photo-1544513566-b4de3719ee70?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" alt="">
+				</a>
+			</div>
+			<div class="news-item">
+				<a href="#">
+					<h3>Media Item</h3>
+					<img src="https://images.unsplash.com/photo-1543398355-bb0d4b80ae22?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2711&q=80" alt="">
+				</a>
+			</div>
+			<div class="news-item">
+				<a href="#">
+					<h3>Media Item</h3>
+					<img src="https://images.unsplash.com/photo-1548183375-5eaf88925e19?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80" alt="">
+				</a>
+			</div>
+			<div class="news-item">
+				<a href="#">
+					<h3>Media Item</h3>
+					<img src="https://images.unsplash.com/photo-1544257750-572358f5da22?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1988&q=80" alt="">
+				</a>
+			</div>
+			<div class="news-item">
+				<a href="#">
+					<h3>Media Item</h3>
+					<img src="https://images.unsplash.com/photo-1473260079709-83c808703435?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2679&q=80" alt="">
+				</a>
+			</div>
+			<div class="news-item">
+				<a href="#">
+					<h3>Media Item</h3>
+					<img src="https://images.unsplash.com/photo-1544513566-b4de3719ee70?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" alt="">
+				</a>
+			</div>
+			<div class="news-item">
+				<a href="#">
+					<h3>Media Item</h3>
+					<img src="https://images.unsplash.com/photo-1543398355-bb0d4b80ae22?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2711&q=80" alt="">
+				</a>
+			</div>
+		</div>
+	</section>
+</main>
+
+<?php get_footer(); ?>
