444dc946d017a6a776cd456bb620b1054580837e
Eric Sherred <eric@ericsherred.com>
2019-08-05 14:59:14 -0500

    better?

--- wp-content/themes/smarter-safer/templates/issues.php
+++ wp-content/themes/smarter-safer/templates/issues.php
@@ -70,47 +70,37 @@
 						get_category_by_slug( 'in-the-news' )->term_id,
 						get_category_by_slug( 'press-release' )->term_id,
 					);
-					$news = new WP_Query(array(
+					$news = get_posts(array(
 						'numberposts' => '6',
 						'cat' => get_field('default_category'),
 						'category__in' => $cats,
 					));
-					$has_posts = false;
-					if($news){
-						foreach($news as $item) {
-							if(has_category(get_field('default_category'), $item->ID)) {
-								$has_posts = true;
-							}
-						}
-					}
 				?>
-				<?php if($has_posts): ?>
+				<?php if($news): ?>
 					<?php foreach($news as $item): ?>
-						<?php if(has_category(get_field('default_category'), $item->ID)) : ?>
-							<?php
-								if(get_the_post_thumbnail_url($item->ID)) {
-									$thumb = get_the_post_thumbnail_url($item->ID);
-								} elseif(has_category('in-the-news', $item->ID) && get_field('default_in_the_news','options')) {
-									$thumb = get_field('default_in_the_news','options');
-								} elseif(has_category('press-release', $item->ID) && get_field('default_press_release','options')) {
-									$thumb = get_field('default_press_release','options');
-								} elseif(has_category('flooding', $item->ID) && get_field('default_flooding','options')) {
-									$thumb = get_field('default_flooding','options');
-								} elseif(has_category('wildfires', $item->ID) && get_field('default_wildfires','options')) {
-									$thumb = get_field('default_wildfires','options');
-								} elseif(has_category('earthquake', $item->ID) && get_field('default_earthquake','options')) {
-									$thumb = get_field('default_earthquake','options');
-								} else {
-									$thumb = get_field('default_image','options');
-								}
-							?>
-							<div class="post">
-								<a href="<?php the_permalink($item->ID) ?>">
-									<h3><?php echo $item->post_title; ?></h3>
-									<img src="<?php echo $thumb ?>" alt="<?php echo $item->post_title; ?>">
-								</a>
-							</div>
-						<?php endif; ?>
+						<?php
+							if(get_the_post_thumbnail_url($item->ID)) {
+								$thumb = get_the_post_thumbnail_url($item->ID);
+							} elseif(has_category('in-the-news', $item->ID) && get_field('default_in_the_news','options')) {
+								$thumb = get_field('default_in_the_news','options');
+							} elseif(has_category('press-release', $item->ID) && get_field('default_press_release','options')) {
+								$thumb = get_field('default_press_release','options');
+							} elseif(has_category('flooding', $item->ID) && get_field('default_flooding','options')) {
+								$thumb = get_field('default_flooding','options');
+							} elseif(has_category('wildfires', $item->ID) && get_field('default_wildfires','options')) {
+								$thumb = get_field('default_wildfires','options');
+							} elseif(has_category('earthquake', $item->ID) && get_field('default_earthquake','options')) {
+								$thumb = get_field('default_earthquake','options');
+							} else {
+								$thumb = get_field('default_image','options');
+							}
+						?>
+						<div class="post">
+							<a href="<?php the_permalink($item->ID) ?>">
+								<h3><?php echo $item->post_title; ?></h3>
+								<img src="<?php echo $thumb ?>" alt="<?php echo $item->post_title; ?>">
+							</a>
+						</div>
 					<?php endforeach; ?>
 				<?php else: ?>
 					More news coming soon!
